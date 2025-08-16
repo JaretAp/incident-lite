ad93b84e64c8b15e95a31eb5d0d33a02776b723e
Jaret <jaretva@gmail.com>
2025-08-16 17:59:16 -0400

    Ignore local secret; use example config

--- app/db.php
+++ app/db.php
@@ -1,10 +1,10 @@
 <?php
 
 $dsn = 'mysql:host=localhost;dbname=incident_lite;charset=utf8mb4';
-$user = 'root'; $pass = '';
+$user = 'incidentapp'; $pass = 'strong_password';
 $options = [
   PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
   PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
   PDO::ATTR_EMULATE_PREPARES => false,
 ];
-$pdo = new PDO($dsn, $user, $pass, $option);
+$pdo = new PDO($dsn, $user, $pass, $options);
---
+++ config/secret.example.php
@@ -0,0 +1,4 @@
+<?php
+define('DB_DSN',  'mysql:host=127.0.0.1;dbname=incident_lite;charset=utf8mb4');
+define('DB_USER', 'incidentapp');
+define('DB_PASS', 'replace_me');
